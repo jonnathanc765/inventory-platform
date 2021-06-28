9bbc1b20db68c7fe89b82b1d348ef191b00314a9
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2021-06-28 15:13:42 -0400

    added theme color for mobile google chrome tab color

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { name: 'theme-color', content: '#35495e' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
