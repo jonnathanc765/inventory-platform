7fee1e28e995cc370851d5a5e0bf91638b96be21
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2021-06-28 11:17:09 -0400

    product card ready but no readed

--- pages/index.vue
+++ pages/index.vue
@@ -222,23 +222,7 @@
         </ul> -->
         <div class="row">
           <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="/images/products/1.jpg" alt="" />
-                <div class="pi-links">
-                  <a href="#" class="add-card">
-                    <i class="flaticon-bag"></i><span>ADD TO CART</span>
-                  </a>
-                  <a href="#" class="wishlist-btn">
-                    <i class="flaticon-heart"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="pi-text">
-                <h6>$35,00</h6>
-                <p>Flamboyant Pink Top</p>
-              </div>
-            </div>
+            <ProductCard></ProductCard>
           </div>
           <div class="col-lg-3 col-sm-6">
             <div class="product-item">
